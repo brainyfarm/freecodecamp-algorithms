2c27705ff04b94431053108e853bb5876de90ede
brainyfarm <brainyfarm@gmail.com>
2016-09-06 07:02:17 +0100

    Updated javascript solution

--- check_for_palindromes/palindrome.js
+++ check_for_palindromes/palindrome.js
@@ -8,6 +8,6 @@ function palindrome(str) {
     // Get rid of non alphanumeric characters
     var cleanedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
     // Compare the cleaned string to iteself in reverse, return true or false
-  	return ( cleanedString === cleanedString.split("").reverse().join("") );
+    return ( cleanedString === cleanedString.split("").reverse().join("") );
 }
 palindrome("Rac_*& eCar")
